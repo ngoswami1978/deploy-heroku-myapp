59ac5aac6f0259acba7cddb54fbff93d849a3f0c
Neeraj Goswami <ngoswami1978@users.noreply.github.com>
2023-08-21 18:12:45 +0530

    commit task to do list 9

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 //dependencies required for the app
 var express = require("express");
 var bodyParser = require("body-parser");
+const { check, validationResult } = require('express-validator')
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+//const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// Set Templating Enginge
 app.set("view engine", "ejs");
+
 //render css files
 app.use(express.static("public"));
 
@@ -30,36 +35,74 @@ var task = [
 , "Time 22:00 to 23:00 AM"
 ];
 //placeholders for removed task
-var complete = ["Finish Task"];
+var complete = [""];
+var errorMsg = [""];
+complete.pop();
+errorMsg.pop();
 var datetime = new Date();
 
 //post route for adding new task 
-app.post("/addtask", function(req, res) {
+app.post("/addtask", function(req, res) {    
     var newTask = req.body.newtask;
     //add the new task from the post route
     task.push(newTask);
-    res.redirect("/");
+    res.redirect("/");    
 });
 
-app.post("/removetask", function(req, res) {
+// app.post('/addtask', urlencodedParser, [
+//     check('username', 'This username must me 3+ characters long')
+//         .exists()
+//         .isLength({ min: 3 }),
+//     check('email', 'Email is not valid')
+//         .isEmail()
+//         .normalizeEmail()
+// ], (req, res)=> {    
+//     const errors = validationResult(req)
+//     if(!errors.isEmpty()) {
+//         // return res.status(422).jsonp(errors.array())
+//         const alert = errors.array()
+//         res.render('addtask', {
+//             alert
+//         })
+//     } 
+//     else{
+//     var newTask = req.body.newtask;
+//     //add the new task from the post route
+//     task.push(newTask);
+//     res.redirect("/"); 
+//     }      
+// })
+
+app.post("/bookedtask", function(req, res) {
+    const errors = validationResult(req)
+    var _room = req.body.selectpicker;
+    console.log('Room Selected  - Room ', _room);
+    if (_room==0) {
+        errorMsg.push("Please Select Room Number");
+        res.redirect("/");
+    }    
+    else{
+    errorMsg.pop();
     var completeTask = req.body.check;
     //check for the "typeof" the different completed task, then add into the complete task
     if (typeof completeTask === "string") {
-        complete.push(completeTask);
+    var _room = req.body.selectpicker;        
+        complete.push(completeTask + "--> Room" + _room);
         //check if the completed task already exits in the task when checked, then remove it
         task.splice(task.indexOf(completeTask), 1);
-    } else if (typeof completeTask === "object") {
+    } else if (typeof completeTask === "object") {        
         for (var i = 0; i < completeTask.length; i++) {
-            complete.push(completeTask[i]);
+            complete.push(completeTask[i]+ "--> Room " + _room);
             task.splice(task.indexOf(completeTask[i]), 1);
         }
     }
     res.redirect("/");
+    }
 });
 
 //render the ejs and display added task, completed task
-app.get("/", function(req, res) {
-    res.render("index", { task: task, complete: complete });
+app.get("/", function(req, res) {    
+    res.render("index", { task: task, complete: complete , errorMsg:errorMsg });
 });
 
 //set app to listen on port 3000
